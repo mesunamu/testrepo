c389aff97e1becde1425a29ec13a522dfa19ea41
mesunamu <msn.murthy@intellectdesign.com>
2017-08-28 10:59:16 -0400

    First Commit

---
+++ demo-module/src/main/java/com/sample/module/app/App.java
@@ -0,0 +1,12 @@
+package com.sample.module.app;
+
+import com.sample.module.util.DateUtil;
+
+public class App {
+
+	//this is to test
+	public static void main(String[] args) {
+		System.out.println("Hello World! Today is " + DateUtil.getToday());
+	}
+
+}
---
+++ util-module/src/main/java/com/sample/module/util/DateUtil.java
@@ -0,0 +1,14 @@
+package com.sample.module.util;
+
+import java.util.Date;
+
+import org.apache.commons.lang.time.DateFormatUtils;
+
+public class DateUtil {
+
+	public static String getToday() {
+		String today = DateFormatUtils.format(new Date(), "dd-MMM-yyyy");
+		return today;
+	}
+
+}
